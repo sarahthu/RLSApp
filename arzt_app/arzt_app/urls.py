8d44b23163290008259af2aac96daf6b0fe7412b
Pia Sälzle <pia.saelzle@gmail.com>
2026-01-21 10:19:47 +0100

    Update urls

--- arzt_app/arzt_app/urls.py
+++ arzt_app/arzt_app/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from portal.views import startseite , home, dokumentation, einstellungen, profil
+from portal.views import startseite , home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
